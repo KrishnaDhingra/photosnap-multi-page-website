322df9736a29d65ff1ec4ce4d12edb1b80318944
Krishna Dhingra <dkrishna1608@gmail.com>
2021-09-06 16:23:55 +0530

    Making the navbar, creating required folders ( components, assets) and installing react-router-dom

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Navbar(){
     return(
-        <p>Hello everyone</p>
+        <div className="navbar bg-white shadow-md flex justify-evenly px-0 lg:px-7 py-4 align-center">
+            <span className="logo text-black text-xl font-black my-auto tracking-wider">PHOTOSNAP</span>
+            <ul className="flex  text-sm font-medium tracking-widest">
+                <li className="mx-6 my-auto"><a href="">STORIES</a></li>
+                <li className="mx-6 my-auto"><a href="">FEATURES</a></li>
+                <li className="mx-6 my-auto"><a href="">PRICING</a></li>
+            </ul>
+            <button className="get_invite px-7 bg-black text-white text-sm font-semibold py-2.5 tracking-widest">GET AN INVITE</button>
+        </div>
     )
 }
 export default Navbar
