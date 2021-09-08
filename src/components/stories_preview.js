ac0b64d0890139a3b260af0a8033b63dd8b08f48
Krishna Dhingra <dkrishna1608@gmail.com>
2021-09-09 01:02:21 +0530

    Adding stories

--- src/components/stories_preview.js
+++ src/components/stories_preview.js
@@ -1,72 +1,72 @@
-import React from 'react'
-import { GetInvite } from './basic_components'
-import { Mountains } from '../assets/images/stories/desktop/mountains.jpg'
+// import React from 'react'
+// import { GetInvite } from './basic_components'
+// import { Mountains } from '../assets/images/stories/desktop/mountains.jpg'
 
-export let Stories_Preview_Container = () =>{
-    return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 py-20 xl:grid-cols-4">
-            <Stories_Preview1/>
-            <Stories_Preview2/>
-            <Stories_Preview3/>
-            <Stories_Preview4/>
+// export let Stories_Preview_Container = () =>{
+//     return(
+//         <div className="grid grid-cols-1 sm:grid-cols-2 py-20 xl:grid-cols-4">
+//             <Stories_Preview1/>
+//             <Stories_Preview2/>
+//             <Stories_Preview3/>
+//             <Stories_Preview4/>
 
-        </div>
-    )
+//         </div>
+//     )
 
-}
-export let Stories_Preview1 = () =>{
-    return(
-        <div className="stories_preview1 bg-blue-200 h-lg flex flex-col p-10 justify-end">
-            <Stories_Heading text={'The Mountains'}/>
-            <Stories_Photographer text={'by John Appleseed'}/>
-            <Divider/>
-            <GetInvite className="">READ MORE</GetInvite>
-        </div>
-    )
+// }
+// export let Stories_Preview1 = () =>{
+//     return(
+//         <div className="stories_preview1 bg-blue-200 h-lg flex flex-col p-10 justify-end">
+//             <Stories_Heading text={'The Mountains'}/>
+//             <Stories_Photographer text={'by John Appleseed'}/>
+//             <Divider/>
+//             <GetInvite className="">READ MORE</GetInvite>
+//         </div>
+//     )
 
-}
-export let Stories_Preview2 = () =>{
-    return(
-        <div className="stories_preview2 bg-blue-200 h-lg flex flex-col p-10 justify-end">
-            <Stories_Heading text={'Sunset Cityscapes'}/>
-            <Stories_Photographer text={'by Benjamin Cruz'}/>
-            <Divider/>
-            <GetInvite className="">READ MORE</GetInvite>
-        </div>
-    )
+// }
+// export let Stories_Preview2 = () =>{
+//     return(
+//         <div className="stories_preview2 bg-blue-200 h-lg flex flex-col p-10 justify-end">
+//             <Stories_Heading text={'Sunset Cityscapes'}/>
+//             <Stories_Photographer text={'by Benjamin Cruz'}/>
+//             <Divider/>
+//             <GetInvite className="">READ MORE</GetInvite>
+//         </div>
+//     )
 
-}
-export let Stories_Preview3 = () =>{
-    return(
-        <div className="stories_preview3 bg-blue-200 h-lg flex flex-col p-10 justify-end">
-            <Stories_Heading text={'18 Days Voyage'}/>
-            <Stories_Photographer text={'by Alexie Borodin'}/>
-            <Divider/>
-            <GetInvite className="">READ MORE</GetInvite>
-        </div>
-    )
+// }
+// export let Stories_Preview3 = () =>{
+//     return(
+//         <div className="stories_preview3 bg-blue-200 h-lg flex flex-col p-10 justify-end">
+//             <Stories_Heading text={'18 Days Voyage'}/>
+//             <Stories_Photographer text={'by Alexie Borodin'}/>
+//             <Divider/>
+//             <GetInvite className="">READ MORE</GetInvite>
+//         </div>
+//     )
 
-}
-export let Stories_Preview4 = () =>{
-    return(
-        <div className="stories_preview4 bg-blue-200 h-lg flex flex-col p-10 justify-end">
-            <Stories_Heading text={'Architecturals'}/>
-            <Stories_Photographer text={'by Samantha Brooke'}/>
-            <Divider/>
-            <GetInvite className="">READ MORE</GetInvite>
-        </div>
-    )
+// }
+// export let Stories_Preview4 = () =>{
+//     return(
+//         <div className="stories_preview4 bg-blue-200 h-lg flex flex-col p-10 justify-end">
+//             <Stories_Heading text={'Architecturals'}/>
+//             <Stories_Photographer text={'by Samantha Brooke'}/>
+//             <Divider/>
+//             <GetInvite className="">READ MORE</GetInvite>
+//         </div>
+//     )
 
-}
-export let Stories_Heading = (props) =>{
-    return <h1 className="text-xl md:text-xl leading-snug font-bold tracking-normal text-white w-full md:w-96">{props.text}</h1>
+// }
+// export let Stories_Heading = (props) =>{
+//     return <h1 className="text-xl md:text-xl leading-snug font-bold tracking-normal text-white w-full md:w-96">{props.text}</h1>
     
-}
-export let Stories_Photographer = (props) =>{
-    return  <p className="text-white font-normal text-sm leading-6 w-full md:w-96 tracking-wide">{props.text}</p>
+// }
+// export let Stories_Photographer = (props) =>{
+//     return  <p className="text-white font-normal text-sm leading-6 w-full md:w-96 tracking-wide">{props.text}</p>
 
-}
-export let Divider = () =>{
-    return <div className="w-full border-t-2 opacity-40 border-gray-300 m-3 ml-0"></div>
-}
+// }
+// export let Divider = () =>{
+//     return <div className="w-full border-t-2 opacity-40 border-gray-300 m-3 ml-0"></div>
+// }
 
