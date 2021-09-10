6e0980f56b2f0e952fbc3d8df07a501e8a6bbc80
Krishna Dhingra <dkrishna1608@gmail.com>
2021-09-10 22:05:15 +0530

    Making the main stories component

--- src/components/data.js
+++ src/components/data.js
@@ -91,31 +91,31 @@
 //     },
 //     {
 //         date: '',
-//         heading: '',
-//         photographer: '',
-//         image: '',
-//     },
-// ]
-// export let preview_stories_data = [
-//     {
-//         heading: 'The Mountains',
-//         photographer: 'by John Appleseed',
-//         image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/mountains.jpg?raw=true'
-//     },
-//     {
-//         heading: 'Sunset Cityscapes',
-//         photographer: 'by Benjamin Cruz',
-//         image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/cityscapes.jpg?raw=true'
-//     },
-//     {
-//         heading: '18 Days Voyage',
-//         photographer: 'by Alexie Borodin',
-//         image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/18-days-voyage.jpg?raw=true'
-//     },
-//     {
-//         heading: 'Architecturals',
-//         photographer: 'by Samantha Brooke',
-//         image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/architecturals.jpg?raw=true'
-//     }
+        heading: '',
+        photographer: '',
+        image: '',
+    },
+]
+export let preview_stories_data = [
+    {
+        heading: 'The Mountains',
+        photographer: 'by John Appleseed',
+        image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/mountains.jpg?raw=true'
+    },
+    {
+        heading: 'Sunset Cityscapes',
+        photographer: 'by Benjamin Cruz',
+        image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/cityscapes.jpg?raw=true'
+    },
+    {
+        heading: '18 Days Voyage',
+        photographer: 'by Alexie Borodin',
+        image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/18-days-voyage.jpg?raw=true'
+    },
+    {
+        heading: 'Architecturals',
+        photographer: 'by Samantha Brooke',
+        image: 'https://github.com/KrishnaDhingra/photosnap_frontendmentor/blob/master/static/images/stories/desktop/architecturals.jpg?raw=true'
+    }
 
-// ]
+]
