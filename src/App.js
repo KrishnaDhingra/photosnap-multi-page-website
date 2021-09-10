e247e620f32140a0c07cb22217d2e026940c6dd5
Krishna Dhingra <dkrishna1608@gmail.com>
2021-09-10 14:58:33 +0530

    Adding react-router-dom functionality

--- src/App.js
+++ src/App.js
@@ -3,21 +3,52 @@ import Navbar from './components/navbar.js'
 import { Home1, Home2, Home3 } from './components/home.js'
 import { Stories_Preview_Container } from './components/stories_preview.js';
 import { FeautresContainer } from './components/features.js'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 
-function App() {
+export let App = () => {
   return (
+    <Router>
 
-    <div className="App">
-      <Navbar></Navbar>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/stories" exact component={Stories}/>
+          <Route path="/features" exact component={Features}/>
+          <Route path="/pricing" exact component={Pricing}/>
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
+
+export let Home = () => {
+  return (
+    <>
       <Home1/>
       <Home2/>
       <Home3/>
       <Stories_Preview_Container/>
       <FeautresContainer/>
-    </div>
-
-  );
+    </>
+  )
+}
+export let Stories = () => {
+  return (
+    <Stories_Preview_Container/>
+  )
 }
 
-export default App;
+export let Features = () => {
+  return(
+    <FeautresContainer/>
+  )
+}
+
+export let Pricing = () => {
+  return(
+    <p>This is pricing</p>
+  )
+}
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Logo } from './basic_components'
+import { Link } from 'react-router-dom'
+
 
 function Navbar(){
     return(
-        <div className="navbar bg-white shadow-md flex justify-between md:justify-evenly px-4 sm:px-8 md:px-0 lg:px-7 py-4 align-center font-body">
-            <Logo/>
+        <div className="navbar bg-white shadow-md flex justify-between md:justify-evenly px-4 sm:px-8 md:px-0 lg:px-7 py-4 items-center font-body">
+            <Link to="/"><Logo/></Link>
             <ul className="hidden md:flex  text-xs font-semibold tracking-widest ">
-                <li className="mx-6 my-auto hover:text-gray-400 duration-300"><a href="">STORIES</a></li>
-                <li className="mx-6 my-auto hover:text-gray-400 duration-300"><a href="">FEATURES</a></li>
-                <li className="mx-6 my-auto hover:text-gray-400 duration-300"><a href="">PRICING</a></li>
+                <Link to="/"><li className="mx-6 my-auto hover:text-gray-400 duration-300"><a href="">STORIES</a></li></Link>
+                <Link to="/"><li className="mx-6 my-auto hover:text-gray-400 duration-300"><a href="">FEATURES</a></li></Link>
+                <Link to="/"><li className="mx-6 my-auto hover:text-gray-400 duration-300"><a href="">PRICING</a></li></Link>
             </ul>
             <button className="hidden md:flex get_invite px-7 bg-black text-white text-sm font-semibold py-2.5 tracking-widest hover:bg-gray-200 hover:text-black duration-500">GET AN INVITE</button>
             <div className="md:hidden flex flex-col justify-around items-center h-3 my-auto">
